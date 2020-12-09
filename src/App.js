c8b9e536aaa128eb82f4f10f1de9ddd72c488584
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-09 09:42:19 -0500

    did it with Jermey yesterday and didn't realize it wasn't complete in my files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+
+
+let movieData = [
+  {
+    title: "The Fellowship of the Ring",
+    hours: "2",
+    minutes: "58"
+  },
+  {
+    title: "The Two Towers",
+    hours: "2",
+    minutes: "59"
+  },
+  {
+    title: "Return of the King",
+    hours: "3",
+    minutes: "21"
+  }
+]
+
+const movies = movieData.map((components, idx) => {
+  return <Movie title={components.title} hours={components.hours} minutes={components.hours} key={idx} />
+});
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies}
     </div>
   );
 }
